2b0c6e435cb2d0e3fb4da945575737c0936a73f5
sz <dummy@example.com>
2023-01-18 01:51:43 +0100

    Debug build & c++latest for prelim. c++23 <format> support.

--- build-static.cmd
+++ build-static.cmd
@@ -7,7 +7,7 @@ rem	runtime DLLs, so trying -MT here for a full static link would be futile...
 rem	(It would require a complete SFML rebuild with -MT from sources locally.)
 
 
-cl -DSFML_STATIC -MD -std:c++20 -EHsc %sz_appname%.cpp -Fo%sz_sfml_test_out%/ -Fe%sz_sfml_test_out%/ ^
+cl -DSFML_STATIC -Zi -MD -std:c++latest -EHsc %sz_appname%.cpp -Fo%sz_sfml_test_out%/ -Fe%sz_sfml_test_out%/ -Fd%sz_sfml_test_out%/ ^
 	sfml-graphics-s.lib sfml-window-s.lib sfml-system-s.lib ^
 	opengl32.lib freetype.lib vorbis.lib vorbisfile.lib ^
 	user32.lib kernel32.lib gdi32.lib winmm.lib advapi32.lib
--- build.cmd
+++ build.cmd
@@ -2,6 +2,6 @@
 
 call _setenv.cmd
 
-cl -std:c++20 -EHsc %sz_appname%.cpp -Fo%sz_sfml_test_out%/ -Fe%sz_sfml_test_out%/ ^
+cl -std:c++latest -EHsc %sz_appname%.cpp -Fo%sz_sfml_test_out%/ -Fe%sz_sfml_test_out%/ -Fd%sz_sfml_test_out%/ ^
 	sfml-graphics.lib sfml-window.lib sfml-system.lib ^
 	opengl32.lib
