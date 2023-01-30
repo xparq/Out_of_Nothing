a48063d62d681fa1a39deb20da8cc214235719b0
sz <lunakid@gmail.com>
2023-01-30 03:30:48 +0100

    Upgrade Args (cmdline proc.) to 1.6

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include "Args.hpp"
 #include "engine_sfml.hpp"
 
-#include <iostream> // cerr
 #include <string>
-#include <cstdlib> // atof
+#include <cstdlib> // atoi
+#include <iostream> // cout
 
 using namespace std;
 
@@ -17,12 +17,12 @@ int main(int argc, char* argv[])
 //============================================================================
 {
 	Args args(argc, argv, {
-// Options with 1 param. don't need to be defined:
-//			{"moons", 1}, // number of moons to start with
+	// Options with 1 param. don't need to be defined:
+	//	{"moons", 1}, // number of moons to start with
 	});
 	//auto exename = args.exename();
 	if (args["?"] || args["h"] || args["help"]) {
-		cout << "Usage: " << args.exename() << " [-V] [--moons n]" << endl;
+		cout << "Usage: " << args.exename() << " [-V] [--moons=n]" << endl;
 		return 0;
 	}
 	if (args["V"]) {
