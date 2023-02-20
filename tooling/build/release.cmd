2e5e0ee5f61f855e6a80d14857dc818cac20c658
sz <lunakid@gmail.com>
2023-02-20 15:28:47 +0100

    Cancelling #133 (bundle MSVC DLLs) - FAILED!

--- tooling/build/release.cmd
+++ tooling/build/release.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-call %~dp0tooling/_setenv.cmd keep_sfml_libroot
+call %~dp0../_setenv.cmd keep_sfml_libroot
 
 pushd %sz_prjdir%
 rem !! Amazingly, this started to fail for some unknown reason: that same git cmd
@@ -16,8 +16,22 @@ set packfile=%sz_release_dir%/%packname%
 rem Some cmds. (like DEL) might fail to see the file without / -> \ conv. :-o
 set "packfile=%packfile:/=\%"
 
+rem !! All these below are assumed to be in %SystemRoot%\System32 (see the actual zip command!):
+rem !! Also: if the last item had a trailing ^ then CMD would gobble up the next command, too,
+rem !! despite the empty line! :-o
+set vcredist_dlls=^
+	vcruntime140.dll^
+	vcruntime140_1.dll^
+	msvcp140.dll^
+	msvcp140_atomic_wait.dll
+	rem !! WTF the _1 gives "zip warning: name not matched" (no matter the ordering/quoting)?! :-o
+	rem !! Same for _1d.dll Plain `DIR` just lists it OK, attributes seem fine... :-o
+	rem !! -S doesn't help either (accordingly).
+	rem !! BUT: if copied locally, it's fine again... :-/ FFS...
+
 set sfml_dlls=openal32.dll
 
+rem Overwrite existing pack?
 if exist "%packfile%" (
 	echo Deleting existing release pack: "%packfile%"...
 	echo Abort now if you want to keep it^!
@@ -25,7 +39,6 @@ if exist "%packfile%" (
 	del "%packfile%"
 )
 if not exist "%sz_release_dir%" md "%sz_release_dir%"
-
 
 pushd "%sz_out_dir%"
 zip %packfile% ./%sz_appname%.exe
@@ -36,10 +49,32 @@ zip %packfile% ./%sfml_dlls%"
 popd
 
 pushd "%sz_prjdir%"
-rem zip -r %packfile% ./%sz_asset_subdir% -x ./%sz_asset_subdir%/music/*.ogg*
 zip -r %packfile% ./%sz_asset_subdir% -x ./%sz_asset_subdir%/music/*.ogg*
 zip -r %packfile% ./%sz_asset_subdir%/music/default.ogg
+
+rem Add the VCREDIST DLLs:
+rem !! Alas, can't just do as above, as ZIP would want to pack with the full
+rem !! paths if vcredist_dlls had full paths... :-/
+pushd "%SystemRoot%\System32"
+rem !! Amazingly, the sh* below was syntactically invalid for CMD with the
+rem !! if ... (...) else (...) syntax, no matter the escaping... :-/
+if     exist "%packfile%" zip -rS %packfile% %vcredist_dlls%
+if not exist "%packfile%" echo. & echo - WARNING: couldn't add the MSVC DLLs ^(%packfile% must be an abs. path^)!
 popd
+
+rem Also the `api-ms-win-crt-*` ones! :-o
+rem !! This didn't help, and not sure it's a good idea to begin with... E.g. Sysinternals' Listdlls doesn't even mention these.
+::pushd "%SystemRoot%\System32"\downlevel
+::if     exist "%packfile%" zip -rS %packfile% api-ms-win-crt-*
+::if not exist "%packfile%" echo. & echo - WARNING: couldn't add the MSVC DLLs ^(%packfile% must be an abs. path^)!
+::popd
+
+popd
+
+set vcruntime140_1=vcruntime140_1.dll
+echo. & echo WTF's wrong with %vcruntime140_1%?!... Trying separately...
+copy "%SystemRoot%\System32\%vcruntime140_1%" "%sz_tmp_dir%\"
+zip -j "%packfile%" "%sz_tmp_dir%\%vcruntime140_1%"
 
 echo.
 echo Release pack %packfile% created.
