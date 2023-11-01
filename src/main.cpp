6b39bc0b8157ad6600b653d0471ac84d441768a2
sz <698307+xparq@users.noreply.github.com>
2023-11-01 03:59:07 +0100

    Fix #231: Empty cmdline arg caused turmoil in std::

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "extern/Args.hpp"
 #include "OON_sfml.hpp"
 
-#include <string> // stoi
+#include <string> // stoi, stoul
 #include <iostream> // cout
 #include <algorithm> // min
 
@@ -12,12 +12,13 @@ using namespace std;
 
 #include "commit_hash.inc" //!! Must be placed somewhere on the INCLUDE path by the build proc.!
 //!!Another "silent extern" hackery:
-bool DEBUG_cfg_show_keycode = false;
+/*EXPORT*/ bool DEBUG_cfg_show_keycode = false;
 
 //============================================================================
 int main(int argc, char* argv[])
 //============================================================================
 {
+try {
 	Args args(argc, argv, {
 	// Options with 1 param. don't need to be defined:
 	//	{"moons", 1}, // number of moons to start with
@@ -34,33 +35,49 @@ int main(int argc, char* argv[])
 		DEBUG_cfg_show_keycode = true;
 	}
 
-	OON_sfml game;/*({
+	/*!! Normalize the config. process:
+	[ ] pass it all in one go to the game
+	[ ] preferably just the config file name!
+		[ ] SimApp should have the cfg loader already;
+			[ ] calling down to the real app impl. (OON)
+			    for any non-generic (i.e.: unknown) option (-> inih, too)
+	!!*/
+	string cfg = args("cfg");
+	if (cfg.empty()) cfg = args("C"); // OK if still empty
+	OON_sfml game(cfg.c_str());/*({
 		.moons = args("moons"),
 		.interat = ...,
 	});*/
+
+#define or
 	if (args["bodies"]) {
 		auto n = stoi(args("bodies")) - 2; // 2 have been pre-created!...
 		game.add_bodies(n < 0 ? 0 : n); // Avoid possible overflow!
-	} if (args["interact"]) {
+	} or if (args["interact"]) {
 		game.interact_all();
-	} if (args["friction"]) {
+	} or if (args["friction"]) {
 		float f = stof(args("friction"));
 		game.get_world().FRICTION = f;
-	} if (args["loopcap"]) {
-		game.iterations.max(stoul(args("loopcap")));
-	} if (args["snd"]) {
+	} or if (args["loopcap"]) {
+		auto loopcap = unsigned long(-1); // no limit
+		try { loopcap = stoul(args("loopcap")); } catch(...) {
+			cerr << "- WRNING: --loopcap ignored! Must be a valid positive int.\n";
+		}
+		game.iterations.max(loopcap);
+	} or if (args["snd"]) {
 		game.audio.enabled(args("snd") != "off");
-	} if (args["zoom"]) {
+	} or if (args["zoom"]) {
 		float factor = stof(args("zoom"));
 		game.zoom(factor);
 	}
+#undef or
 
-	try {
-		game.run();
-	} catch (...) {
-		cerr << "UNHANDLED EXCEPTION!\n";
-		return -1;
-	}
+	game.run();
+
+} catch (...) {
+	cerr << "- UNHANDLED EXCEPTION!\n";
+	return -1;
+}
 
 	cerr << "Profiling stats: " << IPROF_ALL_THREAD_STATS;
 
