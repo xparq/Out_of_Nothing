222f506785fbc2f6a790ac6eb71baed186f82311
sz <698307+xparq@users.noreply.github.com>
2024-02-24 23:09:06 +0100

    Close #557: Warn about no header dep. trk. in nbuild

--- nbuild.cmd
+++ nbuild.cmd
@@ -7,6 +7,13 @@ call %~dp0tooling/_setenv.cmd
 :: The original (BB/sh-scripted) build process with auto-rebuild:
 ::busybox sh %~dp0tooling/build/_auto-rebuild.sh %*
 
+
+:: #557:
+echo.
+echo - WARNING: This build procedure doesn't support header dependency tracking!
+echo.
+
+
 :: The new (NMAKE-driven, CMD-scripted) process (without auto-rebuild yet!):
 %~dp0tooling/diag/wtime.exe nmake /nologo /f tooling\build\nMakefile.msvc %*
 
